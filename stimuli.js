ed17792fe2e36096118182846a0abad66fcd30ab
Nico Biagi <54856105+NicoBiagi@users.noreply.github.com>
2024-07-28 22:05:46 +0100

    Edited html file

---
+++ stimuli.js
@@ -0,0 +1,128 @@
+var test_stimuli = [
+    {
+      "stimulus": "cropped/19_1.jpg"
+    },
+    {
+      "stimulus": "cropped/3_1.jpg"
+    },
+    {
+      "stimulus": "cropped/4_1.jpg"
+    },
+    {
+      "stimulus": "cropped/21_2.jpg"
+    },
+    {
+      "stimulus": "cropped/12_1.jpg"
+    },
+    {
+      "stimulus": "cropped/10_2.jpg"
+    },
+    {
+      "stimulus": "cropped/15_1.jpg"
+    },
+    {
+      "stimulus": "cropped/11_2.jpg"
+    },
+    {
+      "stimulus": "cropped/18_1.jpg"
+    },
+    {
+      "stimulus": "cropped/10_1.jpg"
+    },
+    {
+      "stimulus": "cropped/9_1.jpg"
+    },
+    {
+      "stimulus": "cropped/14_2.jpg"
+    },
+    {
+      "stimulus": "cropped/17_1.jpg"
+    },
+    {
+      "stimulus": "cropped/8_2.jpg"
+    },
+    {
+      "stimulus": "cropped/20_2.jpg"
+    },
+    {
+      "stimulus": "cropped/21_1.jpg"
+    },
+    {
+      "stimulus": "cropped/13_1.jpg"
+    },
+    {
+      "stimulus": "cropped/5_1.jpg"
+    },
+    {
+      "stimulus": "cropped/12_2.jpg"
+    },
+    {
+      "stimulus": "cropped/5_2.jpg"
+    },
+    {
+      "stimulus": "cropped/6_1.jpg"
+    },
+    {
+      "stimulus": "cropped/16_1.jpg"
+    },
+    {
+      "stimulus": "cropped/7_1.jpg"
+    },
+    {
+      "stimulus": "cropped/15_2.jpg"
+    },
+    {
+      "stimulus": "cropped/13_2.jpg"
+    },
+    {
+      "stimulus": "cropped/2_2.jpg"
+    },
+    {
+      "stimulus": "cropped/3_2.jpg"
+    },
+    {
+      "stimulus": "cropped/1_1.jpg"
+    },
+    {
+      "stimulus": "cropped/16_2.jpg"
+    },
+    {
+      "stimulus": "cropped/2_1.jpg"
+    },
+    {
+      "stimulus": "cropped/20_1.jpg"
+    },
+    {
+      "stimulus": "cropped/4_2.jpg"
+    },
+    {
+      "stimulus": "cropped/18_2.jpg"
+    },
+    {
+      "stimulus": "cropped/19_2.jpg"
+    },
+    {
+      "stimulus": "cropped/1_2.jpg"
+    },
+    {
+      "stimulus": "cropped/14_1.jpg"
+    },
+    {
+      "stimulus": "cropped/9_2.jpg"
+    },
+    {
+      "stimulus": "cropped/8_1.jpg"
+    },
+    {
+      "stimulus": "cropped/11_1.jpg"
+    },
+    {
+      "stimulus": "cropped/17_2.jpg"
+    },
+    {
+      "stimulus": "cropped/7_2.jpg"
+    },
+    {
+      "stimulus": "cropped/6_2.jpg"
+    }
+  ]
